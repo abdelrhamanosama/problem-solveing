2f9589d654025c8a7bab6d80e90cce3d3e3c9f77
abdelrhamanosama <121834592+abdelrhamanosama@users.noreply.github.com>
2026-06-10 11:46:51 +0300

    Create 1370-CountNumberOfNiceSubarrays

---
+++ 1370-CountNumberOfNiceSubarrays/1370-CountNumberOfNiceSubarrays.cpp
@@ -0,0 +1,21 @@
+// Last updated: 6/10/2026, 11:46:52 AM
+class Solution {
+public:
+    int numberOfSubarrays(vector<int>& nums, int k) {
+        int i, j = 0, odd = 0, count , total = 0;
+        int n = nums.size();
+        for(i = 0; i < n; i++){
+            if(nums[i]&1)
+            {
+                odd++;
+                if(odd >= k){
+                    count = 1;
+                    while(!(nums[j++]&1)) count++;
+                    total += count;
+                }
+            }
+            else if(odd >= k)  total += count;
+        }
+        return total;
+    }
+};
