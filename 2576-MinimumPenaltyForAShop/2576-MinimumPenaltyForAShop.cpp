b1b6c6c4295d3e7871b864123c62344896dbb28b
abdelrhamanosama <121834592+abdelrhamanosama@users.noreply.github.com>
2026-06-10 11:42:43 +0300

    Create 2576-MinimumPenaltyForAShop

---
+++ 2576-MinimumPenaltyForAShop/2576-MinimumPenaltyForAShop.cpp
@@ -0,0 +1,28 @@
+// Last updated: 6/10/2026, 11:42:43 AM
+class Solution {
+public:
+    int bestClosingTime(string customers) {
+        int N = (customers.size());
+        int y = 0, n = 0;
+        for (auto x : customers) {
+            if (x == 'Y')
+                y++;
+        }
+        // cout<<y<<"\n";
+        int mn = 1e9, idx = 0;
+        for (int i = 0; i <= N; i++) {
+            if (mn > y + n) {
+                mn = y + n;
+                idx = i;
+                // cout<<mn<<"\n";
+            }
+            if (i == N)
+                break;
+            if (customers[i] == 'Y')
+                y--;
+            else
+                n++;
+        }
+        return idx;
+    }
+};
