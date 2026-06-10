6db8e204857969394fe570710f4ff6c32456e0be
abdelrhamanosama <121834592+abdelrhamanosama@users.noreply.github.com>
2026-06-10 11:50:16 +0300

    Create 146-LruCache

---
+++ 146-LruCache/146-LruCache.cpp
@@ -0,0 +1,41 @@
+// Last updated: 6/10/2026, 11:50:16 AM
+class LRUCache {
+public:
+    int cap;
+    list<pair<int,int>> l;
+    unordered_map<int , list<pair<int,int>>::iterator> mp;
+    LRUCache(int capacity) {
+        cap = capacity;
+    }
+    
+    int get(int key) {
+        if(mp.count(key) == 0) return -1;
+        auto it = mp[key];
+        int val = it->second;
+        l.erase(it);
+        l.push_front({key,val});
+        mp[key] = l.begin();
+        return val;
+    }
+    
+    void put(int key, int value) {
+        if(mp.count(key) != 0){
+            l.erase(mp[key]);
+        }  
+        if(l.size() == cap){
+            int k = l.back().first;
+            l.pop_back();
+            mp.erase(k);
+        }
+
+        l.push_front({key,value});
+        mp[key] = l.begin();
+    }
+};
+
+/**
+ * Your LRUCache object will be instantiated and called as such:
+ * LRUCache* obj = new LRUCache(capacity);
+ * int param_1 = obj->get(key);
+ * obj->put(key,value);
+ */
