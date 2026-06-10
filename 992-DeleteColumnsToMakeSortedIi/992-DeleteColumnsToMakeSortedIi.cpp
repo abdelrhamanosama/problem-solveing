c3bc5bcaf36e09fc7cd6f797c0645e168e5b1a24
abdelrhamanosama <121834592+abdelrhamanosama@users.noreply.github.com>
2026-06-10 11:47:29 +0300

    Create 992-DeleteColumnsToMakeSortedIi

---
+++ 992-DeleteColumnsToMakeSortedIi/992-DeleteColumnsToMakeSortedIi.cpp
@@ -0,0 +1,21 @@
+// Last updated: 6/10/2026, 11:47:29 AM
+class Solution {
+public:
+        int minDeletionSize(vector<string>& A) {
+        int res = 0, n = A.size(), m = A[0].length(), i, j;
+        vector<bool> sorted(n - 1, false);
+        for (j = 0; j < m; ++j) {
+            for (i = 0; i < n - 1; ++i) {
+                if (!sorted[i] && A[i][j] > A[i + 1][j]) {
+                    res++;
+                    break;
+                }
+            }
+            if (i < n - 1) continue;
+            for (i = 0; i < n - 1; ++i) {
+                sorted[i] = sorted[i] || A[i][j] < A[i + 1][j];
+            }
+        }
+        return res;
+    }
+};
