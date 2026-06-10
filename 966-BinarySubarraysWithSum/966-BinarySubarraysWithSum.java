c06e1dfa5a6b1b56a592bbccc3a671d515605fb4
abdelrhamanosama <121834592+abdelrhamanosama@users.noreply.github.com>
2026-06-10 11:47:34 +0300

    Create 966-BinarySubarraysWithSum

---
+++ 966-BinarySubarraysWithSum/966-BinarySubarraysWithSum.java
@@ -0,0 +1,17 @@
+// Last updated: 6/10/2026, 11:47:34 AM
+class Solution {
+    public int numSubarraysWithSum(int[] nums, int goal) {
+        int count = 0;
+        for (int i = 0; i < nums.length; i++) {
+            int sum  = 0;
+            for (int j = i; j < nums.length; j++) {
+                sum +=nums[j];
+                if(sum == goal){
+                    count++;
+                }
+
+            }
+        }
+        return  count;
+    }
+}
