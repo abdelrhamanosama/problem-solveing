135f7513c3d6b570b71a8d356e0f4464ec0f5537
abdelrhamanosama <121834592+abdelrhamanosama@users.noreply.github.com>
2026-06-10 11:43:44 +0300

    Create 2160-MinimumOperationsToMakeAUniValueGrid

---
+++ 2160-MinimumOperationsToMakeAUniValueGrid/2160-MinimumOperationsToMakeAUniValueGrid.cpp
@@ -0,0 +1,23 @@
+// Last updated: 6/10/2026, 11:43:44 AM
+#define all(s) s.begin(), s.end()
+class Solution
+{
+public:
+    int minOperations(vector<vector<int>> &grid, int x)
+    {
+        int n = grid.size();
+        int m = grid[0].size();
+        vector<int> v;
+        for(int i =  0 ; i < n; i++)
+            for(int j = 0; j < m;j++) 
+                v.push_back(grid[i][j]);
+        int rem = v[0]%x;
+        for(auto y: v) if(y%x != rem) return -1;
+        sort(all(v));
+        int median = v[v.size()/2];
+        int cnt = 0;
+        for(int i= 0 ; i< v.size(); i++)
+            cnt += abs(median - v[i])/x;
+        return cnt;
+    }
+};
