08548f16c4bc5a5855b57333a1d202802fbd7b80
abdelrhamanosama <121834592+abdelrhamanosama@users.noreply.github.com>
2026-06-10 11:46:50 +0300

    Create 1428-JumpGameIii

---
+++ 1428-JumpGameIii/1428-JumpGameIii.cpp
@@ -0,0 +1,32 @@
+// Last updated: 6/10/2026, 11:46:50 AM
+/* صلِّ على النبي */
+#include <bits/stdc++.h>
+using namespace std;
+// #define int long long
+#define sz(s) (int)(s).size()
+#define all(s) s.begin(), s.end()
+
+class Solution
+{
+private:
+    // اللهم اهدنا يا رب اليك
+    public:
+        bool canReach(vector<int>& arr, int start) {
+            queue<int> q;
+            vector<bool> visited(arr.size()+3, 0);
+            q.push(start);
+            while(!q.empty()){
+                auto x = q.front();
+                q.pop();
+                visited[x] = 1;
+                if(arr[x] == 0) return 1;
+                int y = x-arr[x];
+                int z = x+arr[x];
+                if(y>=0 && !visited[y]) 
+                q.push(y);
+                if(z < arr.size() && !visited[z]) 
+                q.push(z);
+            }
+        return 0;
+    }
+};
