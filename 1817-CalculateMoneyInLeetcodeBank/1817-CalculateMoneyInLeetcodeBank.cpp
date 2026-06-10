a583289d49949c60f22a9114cd730d2f4d732067
abdelrhamanosama <121834592+abdelrhamanosama@users.noreply.github.com>
2026-06-10 11:44:55 +0300

    Create 1817-CalculateMoneyInLeetcodeBank

---
+++ 1817-CalculateMoneyInLeetcodeBank/1817-CalculateMoneyInLeetcodeBank.cpp
@@ -0,0 +1,15 @@
+// Last updated: 6/10/2026, 11:44:56 AM
+class Solution {
+public:
+    int totalMoney(int n) {
+        int weeks = n/7;
+        int days = n%7;
+        int total =  7 *weeks * (weeks -1 ) / 2+ weeks * 28;
+        int start = weeks + 1;
+        for(int i = 0; i < days; i++)
+        {
+            total += start + i;
+        }
+        return total;
+    }
+};
