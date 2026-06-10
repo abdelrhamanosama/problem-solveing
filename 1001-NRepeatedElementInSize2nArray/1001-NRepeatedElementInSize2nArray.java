450633fa4ce270e3bdf4d9b453814b74b06c4eb9
abdelrhamanosama <121834592+abdelrhamanosama@users.noreply.github.com>
2026-06-10 11:47:26 +0300

    Create 1001-NRepeatedElementInSize2nArray

---
+++ 1001-NRepeatedElementInSize2nArray/1001-NRepeatedElementInSize2nArray.java
@@ -0,0 +1,15 @@
+// Last updated: 6/10/2026, 11:47:27 AM
+class Solution {
+    public int repeatedNTimes(int[] A) {
+        Map<Integer, Integer> count = new HashMap();
+        for (int x: A) {
+            count.put(x, count.getOrDefault(x, 0) + 1);
+        }
+
+        for (int k: count.keySet())
+            if (count.get(k) > 1)
+                return k;
+
+        throw null;
+    }
+}
