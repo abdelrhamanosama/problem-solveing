a8d7394c26ca4df0d4f9eaac5ecf721ae3878f30
abdelrhamanosama <121834592+abdelrhamanosama@users.noreply.github.com>
2026-06-10 11:47:58 +0300

    Create 747-MinCostClimbingStairs

---
+++ 747-MinCostClimbingStairs/747-MinCostClimbingStairs.cpp
@@ -0,0 +1,24 @@
+// Last updated: 6/10/2026, 11:47:59 AM
+class Solution {
+    int mem[1005];
+    vector<int> cost;
+    int n;
+public:
+    int minCostClimbingStairs(vector<int>& c) {
+        memset(mem, -1 , sizeof mem);
+        cost = c;
+        n = c.size();
+        int opt1 = dp(0);
+        memset(mem, -1 , sizeof mem);
+        opt1 = min(opt1  ,dp(1));
+        return opt1;
+    }
+    int dp(int idx){
+        if(idx >= n) return 0;
+        if(~mem[idx]) return mem[idx];
+        int opt1 = 1e9, opt2 = 1e9;
+        opt1 = dp(idx + 1) + cost[idx];
+        opt2 = dp(idx + 2) + cost[idx];
+        return mem[idx] = min(opt1,opt2);
+    }
+};
