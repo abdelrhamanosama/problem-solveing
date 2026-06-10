be11619354c561db4ce7092148850bb3123ead12
abdelrhamanosama <121834592+abdelrhamanosama@users.noreply.github.com>
2026-06-10 11:46:19 +0300

    Create 1479-ConstructTargetArrayWithMultipleSums

---
+++ 1479-ConstructTargetArrayWithMultipleSums/1479-ConstructTargetArrayWithMultipleSums.cpp
@@ -0,0 +1,33 @@
+// Last updated: 6/10/2026, 11:46:19 AM
+class Solution {
+public:
+    bool isPossible(vector<int>& target) {
+        long long sum = 0;
+        priority_queue<long long> pq;
+
+        for (int x : target) {
+            sum += x;
+            pq.push(x);
+        }
+
+        while (true) {
+            long long x = pq.top(); 
+            pq.pop();
+            long long rest = sum - x;  // مجموع باقي العناصر
+
+            if (x == 1 || rest == 1)
+                return true;  // دايمًا ممكن نوصل لـ 1
+
+            if (rest == 0 || rest >= x)
+                return false;
+
+            long long newVal = x % rest;
+
+            if (newVal == 0) newVal = rest;
+
+            sum = rest + newVal;
+            pq.push(newVal);
+        }
+        return true;
+    }
+};
