57cba206ecf8376396e7267c931a507ccbd97f5e
abdelrhamanosama <121834592+abdelrhamanosama@users.noreply.github.com>
2026-06-10 11:42:28 +0300

    Create 2721-SumOfDistances

---
+++ 2721-SumOfDistances/2721-SumOfDistances.cpp
@@ -0,0 +1,31 @@
+// Last updated: 6/10/2026, 11:42:29 AM
+class Solution {
+public:
+    vector<long long> distance(vector<int>& nums) {
+        int n = nums.size();
+        vector<long long> ans(n);
+
+        unordered_map<int, long long> cnt, sum;
+
+        // left pass
+        for (int i = 0; i < n; i++) {
+            int x = nums[i];
+            ans[i] += cnt[x] * i - sum[x];
+            cnt[x]++;
+            sum[x] += i;
+        }
+
+        cnt.clear();
+        sum.clear();
+
+        // right pass
+        for (int i = n - 1; i >= 0; i--) {
+            int x = nums[i];
+            ans[i] += sum[x] - cnt[x] * i;
+            cnt[x]++;
+            sum[x] += i;
+        }
+
+        return ans;
+    }
+};
