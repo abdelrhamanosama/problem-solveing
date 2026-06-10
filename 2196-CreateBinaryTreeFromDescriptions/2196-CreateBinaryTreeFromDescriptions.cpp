b1d32951fcde0c5cde1f9a46acba90eebced96bc
abdelrhamanosama <121834592+abdelrhamanosama@users.noreply.github.com>
2026-06-10 16:01:39 +0300

    File updated at 6/10/2026, 4:01:39 PM

--- 2196-CreateBinaryTreeFromDescriptions/2196-CreateBinaryTreeFromDescriptions.cpp
+++ 2196-CreateBinaryTreeFromDescriptions/2196-CreateBinaryTreeFromDescriptions.cpp
@@ -1,71 +1,69 @@
-// Last updated: 6/10/2026, 3:41:57 PM
-1class Solution
-2{
-3    // struct TreeNode {
-4    //     int val;
-5    //     TreeNode *left;
-6    //     TreeNode *right;
-7    //     TreeNode() : val(0), left(nullptr), right(nullptr) {}
-8    //     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
-9    //     TreeNode(int x, TreeNode *left, TreeNode *right)
-10    //         : val(x), left(left), right(right) {}
-11    // };
-12
-13private:
-14
-15    void dfs(int root, TreeNode* tree,
-16             unordered_map<int, vector<pair<int,int>>>& adj)
-17    {
-18        for(auto [node, left] : adj[root])
-19        {
-20            TreeNode* subtree = new TreeNode(node);
-21
-22            dfs(node, subtree, adj);
-23
-24            if(left)
-25                tree->left = subtree;
-26            else
-27                tree->right = subtree;
-28        }
-29    }
-30
-31public:
-32    TreeNode* createBinaryTree(vector<vector<int>>& descriptions)
-33    {
-34        unordered_map<int, vector<pair<int,int>>> adj;
-35        unordered_map<int,int> indeg;
-36
-37        set<int> nodes;
-38
-39        for(auto &d : descriptions)
-40        {
-41            int u = d[0];
-42            int v = d[1];
-43            int left = d[2];
-44
-45            adj[u].push_back({v,left});
-46
-47            indeg[v]++;
-48
-49            nodes.insert(u);
-50            nodes.insert(v);
-51        }
-52
-53        int root = -1;
-54
-55        for(auto x : nodes)
-56        {
-57            if(indeg[x] == 0)
-58            {
-59                root = x;
-60                break;
-61            }
-62        }
-63
-64        TreeNode* tree = new TreeNode(root);
-65
-66        dfs(root, tree, adj);
-67
-68        return tree;
-69    }
-70};
+// Last updated: 6/10/2026, 4:01:39 PM
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* createBinaryTree(vector<vector<int>>& descriptions) {
+
+        static const auto fast_io = [](){
+            std::ios::sync_with_stdio(false);
+            std::cin.tie(nullptr);
+            return nullptr;
+        }();
+
+        static TreeNode nodes[100001] = {};
+        static int is_child[100001] = { false };
+        static int generation[100001] = { 0 };
+        static int generation_id = 0;
+
+        ++generation_id;
+
+        for(const auto& v : descriptions){
+            const int parent = v[0], child = v[1];
+            const bool left_child = v[2];
+            if(generation[parent] != generation_id){
+                TreeNode* temp = &nodes[parent];
+                temp->left = temp->right = nullptr;
+                temp->val = parent;
+                generation[parent] = generation_id;
+            }
+            if(generation[child] != generation_id){
+                TreeNode* temp = &nodes[child];
+                temp->left = temp->right = nullptr;
+                temp->val = child;
+                generation[child] = generation_id;
+            }
+            TreeNode* parent_node = &nodes[parent];
+            TreeNode* child_node = &nodes[child];
+            if(left_child){
+                parent_node->left = child_node;
+            } else {
+                parent_node->right = child_node;
+            }
+            is_child[child] = true;
+        }
+        int root = -1;
+        for(const auto& v : descriptions){
+            const int parent = v[0];
+            if(!is_child[parent]){
+                root = parent;
+                break;
+            }
+        }
+        for(const auto& v : descriptions){
+            const int child = v[1];
+            is_child[child] = false;
+        }
+
+        return &nodes[root];
+    }
+};
