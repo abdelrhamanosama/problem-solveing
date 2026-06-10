f0fce9afad86474722ec613320a85cb926ee5cb8
abdelrhamanosama <121834592+abdelrhamanosama@users.noreply.github.com>
2026-06-10 11:44:45 +0300

    Create 1877-FindFollowersCount

---
+++ 1877-FindFollowersCount/1877-FindFollowersCount.sql
@@ -0,0 +1,6 @@
+-- Last updated: 6/10/2026, 11:44:46 AM
+# Write your MySQL query statement below
+select user_id , count(user_id) as followers_count
+from Followers
+group by user_id
+order by user_id asc;
