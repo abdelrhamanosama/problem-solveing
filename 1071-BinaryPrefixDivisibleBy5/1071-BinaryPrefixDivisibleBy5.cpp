648b47fc56fccec394334458e12cb7452b723ca2
abdelrhamanosama <121834592+abdelrhamanosama@users.noreply.github.com>
2026-06-10 11:47:21 +0300

    Create 1071-BinaryPrefixDivisibleBy5

---
+++ 1071-BinaryPrefixDivisibleBy5/1071-BinaryPrefixDivisibleBy5.cpp
@@ -0,0 +1,14 @@
+// Last updated: 6/10/2026, 11:47:22 AM
+class Solution {
+public:
+    vector<bool> prefixesDivBy5(vector<int>& nums) {
+        vector<bool> answer;
+        int prefix = 0;
+        int length = nums.size();
+        for (int i = 0; i < length; i++) {
+            prefix = ((prefix << 1) + nums[i]) % 5;
+            answer.emplace_back(prefix == 0);
+        }
+        return answer;
+    }
+};
