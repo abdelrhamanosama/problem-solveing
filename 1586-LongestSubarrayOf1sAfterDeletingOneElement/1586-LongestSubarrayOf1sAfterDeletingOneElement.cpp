d881c06d75d26b546fbceafe009cdfec8d8070d9
abdelrhamanosama <121834592+abdelrhamanosama@users.noreply.github.com>
2026-06-10 11:45:52 +0300

    Create 1586-LongestSubarrayOf1sAfterDeletingOneElement

---
+++ 1586-LongestSubarrayOf1sAfterDeletingOneElement/1586-LongestSubarrayOf1sAfterDeletingOneElement.cpp
@@ -0,0 +1,34 @@
+// Last updated: 6/10/2026, 11:45:52 AM
+class Solution {
+public:
+int longestSubarray(vector<int> &nums)
+{
+    int counter = 0, maxvalue = 0, flag = 0, i, j;
+    for ( i = 0, j = 0; j < nums.size(); j++)
+    {
+        if (nums[j] == 0)
+            counter++;
+        // cout<<maxvalue << " " << i<<" "<< j<<" " <<counter<<endl;
+        
+        if (counter > 1)
+        {
+            maxvalue = max(maxvalue, j - i - 1);
+
+            while (counter > 1)
+            {
+                if (nums[i] == 0)
+                    counter--;
+                i++;
+            }
+            flag = 1;
+        }
+        // cout<<maxvalue << " " << i<<" "<< j<<" " <<counter<<endl;
+    }
+    if(counter < 2)
+    maxvalue = max(maxvalue, j - i - 1);
+    if (counter == 0 || !flag)
+        return nums.size() - 1;
+    return maxvalue;
+}
+
+};
