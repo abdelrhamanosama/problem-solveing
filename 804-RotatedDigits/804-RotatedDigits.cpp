f19e0aca5b0b000b62388d112d50c1c338a89880
abdelrhamanosama <121834592+abdelrhamanosama@users.noreply.github.com>
2026-06-10 11:47:48 +0300

    Create 804-RotatedDigits

---
+++ 804-RotatedDigits/804-RotatedDigits.cpp
@@ -0,0 +1,31 @@
+// Last updated: 6/10/2026, 11:47:48 AM
+/* صلِّ على النبي */
+#include <bits/stdc++.h>
+using namespace std;
+// #define int long long
+#define sz(s) (int)(s).size()
+#define all(s) s.begin(), s.end()
+class Solution
+{
+private:
+int plindrome(int x)
+    {
+        string s = to_string(x);
+        bool f1 = s.find('2') == string::npos && s.find('5') == string::npos && s.find('6') == string::npos && s.find('9') == string::npos;
+        bool f2 = (s.find('3') != string::npos || s.find('7') != string::npos || s.find('4') != string::npos );
+        cout<<s<<"\t"<<f1 <<" \t"<<f2<<"\n";
+        if (f1) return 0;
+        if(f2) return 0;
+        return 1;
+    }
+public:
+    int rotatedDigits(int n)
+    {
+        int cnt = 0;
+        for (int i = 2; i <= n; i++)
+        {
+            cnt += plindrome((i));
+        }
+        return cnt;
+    }
+};
