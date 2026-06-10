f5f942f5bb6eb04e98834b45c101434f1bace19f
abdelrhamanosama <121834592+abdelrhamanosama@users.noreply.github.com>
2026-06-10 11:46:27 +0300

    Create 1476-CountNegativeNumbersInASortedMatrix

---
+++ 1476-CountNegativeNumbersInASortedMatrix/1476-CountNegativeNumbersInASortedMatrix.cpp
@@ -0,0 +1,29 @@
+// Last updated: 6/10/2026, 11:46:27 AM
+class Solution {
+public:
+#define all(s) s.begin(),s.end()
+
+    int countNegatives(vector<vector<int>>& grid) {
+       int cnt = 0;
+       for(int i = 0 ; i < grid.size(); i++){
+            // auto x = upper_bound(all(grid[i]) , -1)-grid[i].begin();
+            // cout<<x<<"\t"<<(upper_bound(all(grid[i]) , -1)==grid[i].end())<<"\n";
+            cnt +=getI(grid[i]); 
+       } 
+       return cnt;
+    }
+    int getI(vector<int> x){
+        int l = 0 , r = x.size()-1, cnt = x.size();
+        while(l<=r){
+            int m = (l+r)/2;
+            if(x[m] <= -1){
+                r = m -1;
+                cnt = m;
+            }
+            else{
+                l = m +1;
+            }
+        }
+        return x.size() - cnt;
+    }
+};
