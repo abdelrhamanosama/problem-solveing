01fdc62d3a3d01f3b5cd4d684ea0adc7b1459a63
abdelrhamanosama <121834592+abdelrhamanosama@users.noreply.github.com>
2026-06-10 11:41:56 +0300

    Create 3493-MaximumNumberOfOperationsToMoveOnesToTheEnd

---
+++ 3493-MaximumNumberOfOperationsToMoveOnesToTheEnd/3493-MaximumNumberOfOperationsToMoveOnesToTheEnd.cpp
@@ -0,0 +1,27 @@
+// Last updated: 6/10/2026, 11:41:57 AM
+class Solution {
+public:
+    int maxOperations(string s) {
+    int i = 0; 
+    int ans = 0;
+    int n = s.size();
+    int ones = 0;
+    bool moved = 0;
+    for(; i < n ; i++){
+        if(s[i] == '1'){
+            ones++;
+            moved = 1;
+            continue;
+        }
+        if(moved){
+            int z = 0;
+            for(int j = i;j < n; j++)
+                if( s[j] == '1') break;
+                else z++;
+            moved = 0;
+            ans += ones;
+        }
+    }
+    return ans++;
+}
+};
