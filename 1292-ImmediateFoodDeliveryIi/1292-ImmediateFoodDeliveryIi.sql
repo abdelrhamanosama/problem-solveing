4d4dfe027df67d3851de585278288eb7f05336ba
abdelrhamanosama <121834592+abdelrhamanosama@users.noreply.github.com>
2026-06-10 11:47:02 +0300

    Create 1292-ImmediateFoodDeliveryIi

---
+++ 1292-ImmediateFoodDeliveryIi/1292-ImmediateFoodDeliveryIi.sql
@@ -0,0 +1,18 @@
+-- Last updated: 6/10/2026, 11:47:03 AM
+# Write your MySQL query statement below
+select 
+    round(sum(
+        case when d.customer_id = t.customer_id and t.diff = 0 and d.order_date  = t.mnd then 1 else 0 end
+    )*1.0/count( distinct d.customer_id )*100,2)
+    as immediate_percentage 
+from 
+    Delivery as d , (
+select
+    customer_id,datediff(min(customer_pref_delivery_date) ,min(order_date))  as diff ,
+    min(order_date)  as mnd
+from 
+    Delivery 
+group by customer_id 
+
+     )as t 
+
