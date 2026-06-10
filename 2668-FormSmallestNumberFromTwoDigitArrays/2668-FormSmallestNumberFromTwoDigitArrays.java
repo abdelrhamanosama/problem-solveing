8016d6cca58210cefcfebe61d2fb032166c20151
abdelrhamanosama <121834592+abdelrhamanosama@users.noreply.github.com>
2026-06-10 11:42:32 +0300

    Create 2668-FormSmallestNumberFromTwoDigitArrays

---
+++ 2668-FormSmallestNumberFromTwoDigitArrays/2668-FormSmallestNumberFromTwoDigitArrays.java
@@ -0,0 +1,27 @@
+// Last updated: 6/10/2026, 11:42:32 AM
+class Solution {
+    public int minNumber(int[] nums1, int[] nums2) {
+        int freq[] = new int[10];
+        int min1 = 10, min2 = 10, min3 = 10;
+        for(int i = 0; i < nums1.length; i++) {
+            freq[nums1[i]]++;
+            if(min1 > nums1[i])
+                    min1 = nums1[i];
+        }
+        for(int i = 0; i < nums2.length; i++) {
+            freq[nums2[i]]++;
+            if(min2 > nums2[i])
+                    min2 = nums2[i];
+        }
+        for(int i = 0; i <= 9; i++) {
+            if(freq[i]==2) {
+                if(min3 > i)
+                    min3 = i;
+            }
+        }
+        if(min3 != 10) return min3;
+        int num1 = min1*10 + min2;
+        int num2 = min2*10 + min1;
+        return (num1 < num2)? num1 : num2;
+    }
+}
