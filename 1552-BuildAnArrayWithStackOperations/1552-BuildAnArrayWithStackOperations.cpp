ef35f5e822f173ceed70b7f75c05cd1b797bb283
abdelrhamanosama <121834592+abdelrhamanosama@users.noreply.github.com>
2026-06-10 11:46:01 +0300

    Create 1552-BuildAnArrayWithStackOperations

---
+++ 1552-BuildAnArrayWithStackOperations/1552-BuildAnArrayWithStackOperations.cpp
@@ -0,0 +1,18 @@
+// Last updated: 6/10/2026, 11:46:02 AM
+class Solution {
+public:
+    vector<string> buildArray(vector<int>& target, int n) {
+        vector<string> ops;
+        int j = 0; // pointer in target
+        
+        for (int i = 1; i <= n && j < target.size(); i++) {
+            ops.push_back("Push");
+            if (i == target[j]) {
+                j++; // number matches target
+            } else {
+                ops.push_back("Pop"); // remove it
+            }
+        }
+        return ops;
+    }
+};
