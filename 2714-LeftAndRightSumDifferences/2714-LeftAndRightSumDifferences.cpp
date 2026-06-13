1fc2ec6bd504821979151fb92484634fbd280b73
abdelrhamanosama <121834592+abdelrhamanosama@users.noreply.github.com>
2026-06-13 14:42:01 +0300

    Create 2714-LeftAndRightSumDifferences

---
+++ 2714-LeftAndRightSumDifferences/2714-LeftAndRightSumDifferences.cpp
@@ -0,0 +1,19 @@
+// Last updated: 6/13/2026, 2:42:01 PM
+#define sz(s) (int)(s).size()
+
+class Solution {
+public:
+    
+    vector<int> leftRightDifference(vector<int>& nums) {
+      int n = sz(nums);
+      vector<int> left(n,0) , right(n,0) , ans(n);   
+      for(int i = 1 ; i  < n; i ++){
+        left[i] = left[i-1] + nums[i-1];
+      }
+      for(int i = n-2 ; i >= 0 ; i --){
+        right[i] = right[i+1] + nums[i+1];
+      }
+      for(int i = 0 ; i < n;  i ++) ans[i] = abs(left[i] - right[i]);
+      return ans;
+    }
+};
