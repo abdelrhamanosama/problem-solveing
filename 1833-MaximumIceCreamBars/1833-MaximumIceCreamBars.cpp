3186e638cb2ef52673c56b8cbb1e0185e9b93547
abdelrhamanosama <121834592+abdelrhamanosama@users.noreply.github.com>
2026-06-22 00:58:57 +0300

    File updated at 6/22/2026, 12:58:56 AM

--- 1833-MaximumIceCreamBars/1833-MaximumIceCreamBars.cpp
+++ 1833-MaximumIceCreamBars/1833-MaximumIceCreamBars.cpp
@@ -1,15 +1,22 @@
-// Last updated: 6/22/2026, 12:41:23 AM
+// Last updated: 6/22/2026, 12:58:56 AM
 1class Solution {
 2public:
 3    int maxIceCream(vector<int>& costs, int coins) {
-4        multiset<int>ms;
-5        for(auto x:costs) ms.insert(x);
-6        int cnt = 0;
-7        while(!ms.empty()&&coins >= *ms.begin()) {
-8          cnt++;
-9          coins-=*ms.begin();
-10          ms.erase(ms.begin());
-11        }
-12        return cnt;
-13    }
-14};
+4        long long arr[100002] = { };
+5        long long sum = 0,cnt = 0;
+6        for(int x : costs) arr[x]++;
+7        for(int i = 1 ; i <= 100000; i++) {
+8            if(sum + arr[i]*i <= coins){
+9              sum+=(arr[i]*i);
+10              cnt+=arr[i];
+11            }
+12            else if(floor((coins-sum)/i) >0) {
+13              cnt+=floor((coins-sum)/i);
+14              break;
+15            }
+16            else 
+17              break;
+18        }
+19        return cnt;
+20    }
+21};
