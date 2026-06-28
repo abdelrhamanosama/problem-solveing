6e00cedb3c16867e836306134cb0bb346db4807d
abdelrhamanosama <121834592+abdelrhamanosama@users.noreply.github.com>
2026-06-28 18:30:15 +0300

    File updated at 6/28/2026, 6:30:15 PM

--- 1846-MaximumElementAfterDecreasingandRearranging/1846-MaximumElementAfterDecreasingandRearranging.cpp
+++ 1846-MaximumElementAfterDecreasingandRearranging/1846-MaximumElementAfterDecreasingandRearranging.cpp
@@ -1,21 +1,18 @@
-// Last updated: 6/28/2026, 6:25:51 PM
-1class Solution {
-2public:
-3        int maximumElementAfterDecrementingAndRearranging(vector<int>& arr) {
-4      multiset<int> s;
-5      for(auto x: arr) s.insert(x);
-6      int mx = 0;
-7      
-8      while(!s.empty()){
-9          auto it = s.lower_bound(mx);
-10          int val = *it;
-11          if(val != mx) {
-12            mx ++;
-13
-14          }
-15          s.erase(it);
-16      }
-17      return mx;
-18    }
-19  
-20};
+// Last updated: 6/28/2026, 6:30:15 PM
+class Solution {
+public:
+    int maximumElementAfterDecrementingAndRearranging(vector<int>& arr) {
+        int n = arr.size();
+        vector<int> freq(n + 1,0);
+        for(int i =0;i<n;i++){
+            freq[min(arr[i],n)]++;
+        }
+        int ans = 0;
+        for(int i =1;i<=n;i++){
+            if(freq[i] == 0) continue;
+
+            ans = min(ans + freq[i],i);
+        }
+        return ans;
+    }
+};
