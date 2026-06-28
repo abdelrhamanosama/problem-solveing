c3f95f06324e4d5801ae4a5fd9f1185badf2f635
abdelrhamanosama <121834592+abdelrhamanosama@users.noreply.github.com>
2026-06-28 18:24:34 +0300

    Create 1846-MaximumElementAfterDecreasingandRearranging

---
+++ 1846-MaximumElementAfterDecreasingandRearranging/1846-MaximumElementAfterDecreasingandRearranging.cpp
@@ -0,0 +1,21 @@
+// Last updated: 6/28/2026, 6:24:34 PM
+1class Solution {
+2public:
+3        int maximumElementAfterDecrementingAndRearranging(vector<int>& arr) {
+4      multiset<int> s;
+5      for(auto x: arr) s.insert(x);
+6      int mx = 0;
+7      
+8      while(!s.empty()){
+9          auto it = s.lower_bound(mx);
+10          int val = *it;
+11          if(val != mx) {
+12            mx ++;
+13
+14          }
+15          s.erase(it);
+16      }
+17      return mx;
+18    }
+19  
+20};
