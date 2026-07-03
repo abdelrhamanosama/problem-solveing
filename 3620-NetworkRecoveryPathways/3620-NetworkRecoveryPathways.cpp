8c6652b3a8b20d37a22d756c5a2c1aaff449a8a8
abdelrhamanosama <121834592+abdelrhamanosama@users.noreply.github.com>
2026-07-03 08:12:58 +0300

    File updated at 7/3/2026, 8:12:58 AM

--- 3620-NetworkRecoveryPathways/3620-NetworkRecoveryPathways.cpp
+++ 3620-NetworkRecoveryPathways/3620-NetworkRecoveryPathways.cpp
@@ -1,4 +1,4 @@
-// Last updated: 7/3/2026, 8:11:48 AM
+// Last updated: 7/3/2026, 8:12:58 AM
 1class Solution {
 2public:
 3    
@@ -7,57 +7,59 @@
 6  {
 7    const int N = 5e4 + 5;
 8    int n = online.size() - 1;
-9    vector<int> indeg(n+1,0);
-10    vector<pair<int, int>> adj[N];
-11    for (auto x : edges)
-12    {
-13      auto u = x[0], v = x[1], c = x[2];
-14      adj[u].push_back({v, c});
-15      indeg[v]++;
-16    }
-17    vector<int> topo;
-18    if (online[0] == 0 || n == 0)
-19      return -1;
-20    queue<int>q;
-21    for(int i = 0 ; i <= n; i++){
-22      if(indeg[i] == 0) q.push(i);
-23    }    
-24    while(!q.empty()){
-25      topo.push_back(q.front());
-26      int node = q.front();
-27      q.pop();
-28      for(auto [x,c]: adj[node])
-29        {
-30          if(--indeg[x] == 0) q.push(x);
-31        }
-32    }
-33    auto check = [&](int lim)->bool{
-34      vector<long long> dist(n+1 , 1e14);
-35      dist[0] = 0;
-36      for(int u : topo){
-37        if(dist[u] == 1e14 || !online[u]) continue;
-38        for(auto [v,c] : adj[u]){
-39            if(c < lim) continue;
-40            if(!online[v]) continue;
-41            dist[v] = min(dist[v], dist[u] + c);
-42        }
-43      }
-44      return dist[n] <= k;
-45    };
-46    int l = 0;
-47    int r = 1e9;
-48    int ans = -1;
-49    while(l <=  r){
-50      int mid = l+r >> 1;
-51      if(check(mid)){
-52        ans = mid;
-53        l = mid +1;
-54      }
-55      else
-56        r = mid - 1;
-57    }
-58      return ans;
-59
-60  }
+9    int mx = 0;
+10    vector<int> indeg(n+1,0);
+11    vector<pair<int, int>> adj[N];
+12    for (auto x : edges)
+13    {
+14      auto u = x[0], v = x[1], c = x[2];
+15      adj[u].push_back({v, c});
+16      mx = max(mx ,c);
+17      indeg[v]++;
+18    }
+19    vector<int> topo;
+20    if (online[0] == 0 || n == 0)
+21      return -1;
+22    queue<int>q;
+23    for(int i = 0 ; i <= n; i++){
+24      if(indeg[i] == 0) q.push(i);
+25    }    
+26    while(!q.empty()){
+27      topo.push_back(q.front());
+28      int node = q.front();
+29      q.pop();
+30      for(auto [x,c]: adj[node])
+31        {
+32          if(--indeg[x] == 0) q.push(x);
+33        }
+34    }
+35    auto check = [&](int lim)->bool{
+36      vector<long long> dist(n+1 , 1e14);
+37      dist[0] = 0;
+38      for(int u : topo){
+39        if(dist[u] == 1e14 || !online[u]) continue;
+40        for(auto [v,c] : adj[u]){
+41            if(c < lim) continue;
+42            if(!online[v]) continue;
+43            dist[v] = min(dist[v], dist[u] + c);
+44        }
+45      }
+46      return dist[n] <= k;
+47    };
+48    int l = 0;
+49    int r = mx;
+50    int ans = -1;
+51    while(l <=  r){
+52      int mid = l+r >> 1;
+53      if(check(mid)){
+54        ans = mid;
+55        l = mid +1;
+56      }
+57      else
+58        r = mid - 1;
+59    }
+60      return ans;
 61
-62};
+62  }
+63
+64};
