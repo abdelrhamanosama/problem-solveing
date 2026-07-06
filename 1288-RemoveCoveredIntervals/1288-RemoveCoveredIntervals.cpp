c8eb8d5cd21c7f3cd6defbc67178ec9d8dad0de5
abdelrhamanosama <121834592+abdelrhamanosama@users.noreply.github.com>
2026-07-06 06:43:25 +0300

    File updated at 7/6/2026, 6:43:24 AM

--- 1288-RemoveCoveredIntervals/1288-RemoveCoveredIntervals.cpp
+++ 1288-RemoveCoveredIntervals/1288-RemoveCoveredIntervals.cpp
@@ -1,18 +1,18 @@
-// Last updated: 7/6/2026, 6:33:57 AM
+// Last updated: 7/6/2026, 6:43:24 AM
 1class Solution {
-2public:
-3int removeCoveredIntervals(vector<vector<int>>& intervals) {
-4      int n = intervals.size();
-5      vector<int> live(n , 1);
-6      for(int i = 0 ; i < n; i++) {
-7        for(int j = 0 ; j < n; j++){
-8          if(i==j) continue;
-9          if(intervals[i][0] <= intervals[j][0]  && intervals[i][1] >= intervals[j][1])
-10            live[j] = 0;
-11        }
-12      }
-13      int cnt = 0;
-14      for(auto x:live) cnt+=x;
-15      return cnt;
+2#define all(s) s.begin(), s.end()
+3
+4public:
+5int removeCoveredIntervals(vector<vector<int>>& intervals) {
+6     ranges::sort(intervals, {}, [](auto& x) {
+7            return pair{x[0], -x[1]};
+8        });
+9      int cnt = 0;
+10      int mx = 0;
+11      for(int i = 0 ; i < intervals.size() ; i++){
+12        cnt+= mx >= intervals[i][1];
+13        mx = max(mx  , intervals[i][1]);
+14      }
+15      return intervals.size()-cnt;
 16    }
 17};
