2e2652fbb8e22536a81b252f8b5a2b90c741f499
abdelrhamanosama <121834592+abdelrhamanosama@users.noreply.github.com>
2026-07-13 13:12:29 +0300

    File updated at 7/13/2026, 1:12:30 PM

--- 3756-ConcatenateNon-ZeroDigitsandMultiplybySumII/3756-ConcatenateNon-ZeroDigitsandMultiplybySumII.cpp
+++ 3756-ConcatenateNon-ZeroDigitsandMultiplybySumII/3756-ConcatenateNon-ZeroDigitsandMultiplybySumII.cpp
@@ -1,37 +1,55 @@
-// Last updated: 7/13/2026, 12:35:59 PM
-1const int MOD = 1e9 + 7;
-2const int MAX_N = 100001;
-3vector<long long> pow10(MAX_N, 0);
-4// init runs only once for all test cases
-5int init = []() {
-6    pow10[0] = 1;
-7    for (int i = 1; i < MAX_N; ++i) {
-8        pow10[i] = (pow10[i - 1] * 10) % MOD;
-9    }
-10    return 0;
-11}();
-12class Solution {
-13public:
-14    vector<int> sumAndMultiply(string s, vector<vector<int>>& queries) {
-15        int n = s.size(), m = queries.size();
-16        vector<long long> sum1(n + 1, 0), sum2(n + 1, 0), cnt(n + 1, 0);
-17        vector<int> res(m, 0);
-18        for (int i = 0; i < n; i++) {
-19            sum1[i + 1] = (sum1[i] + s[i] - '0') % MOD;
-20            sum2[i + 1] =
-21                (s[i] - '0') > 0 ? (sum2[i] * 10 + s[i] - '0')%MOD : sum2[i];
-22            cnt[i + 1] = (s[i] - '0') > 0 ? cnt[i] + 1 : cnt[i];
-23        }
-24        for (int i = 0; i < m; i++) {
-25            int l = queries[i][0];
-26            int r = queries[i][1] + 1;
-27            long long len = cnt[r] - cnt[l];
-28            long long val1 = (sum2[r] - sum2[l] * pow10[len] % MOD + MOD) % MOD;
-29            long long val2 = (sum1[r] - sum1[l]) % MOD;
-30            // cout << sum2[r] << "\t" << sum2[l] << "\t" << len << "\t";
-31            // cout << val1 << "\t" << val2 << "\n";
-32            res[i] = ((val1 * val2) % MOD);
-33        }
-34        return res;
-35    }
-36};
+// Last updated: 7/13/2026, 1:12:30 PM
+1class Solution {
+2#define all(s) s.begin(), s.end()
+3
+4public:
+5    struct dsu {
+6        vector<int> p, s;
+7        dsu(int n) {
+8
+9            //   s = vector<int> (n , 1);
+10            p = vector<int>(n, 1);
+11            for (int i = 0; i < n; i++) {
+12                p[i] = i;
+13            }
+14        }
+15        int find(int u) {
+16            if (u == p[u])
+17                return u;
+18            return p[u] = find(p[u]);
+19        }
+20        void merge(int u, int v) {
+21            int l1 = find(u);
+22            int l2 = find(v);
+23
+24            if (l1 == l2) {
+25
+26                return;
+27            }
+28            if (l1 > l2)
+29                swap(l1, l2);
+30            p[l2] = l1;
+31            //   s[l1]+=s[l2];
+32        }
+33        bool is_connected(int u, int v) { return find(u) == find(v); }
+34    };
+35
+36    vector<bool> pathExistenceQueries(int n, vector<int>& nums, int maxDiff,
+37                                      vector<vector<int>>& queries) {
+38        dsu d(n);
+39        vector<pair<int, int>> v;
+40        for (int i = 0; i < n; i++) {
+41            v.push_back({nums[i], i});
+42        }
+43        sort(v.begin(), v.end());
+44
+45        for (int i = 1; i < n; i++) {
+46            if (v[i].first - v[i - 1].first <= maxDiff)
+47                d.merge(v[i].second, v[i - 1].second);
+48        }
+49        vector<bool> ans;
+50        for (auto x : queries)
+51            ans.push_back(d.is_connected(x[0], x[1]));
+52        return ans;
+53    }
+54};
