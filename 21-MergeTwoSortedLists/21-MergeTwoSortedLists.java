ba7753234aef6bf5a44a5dee8910664432e53e07
abdelrhamanosama <121834592+abdelrhamanosama@users.noreply.github.com>
2026-08-10 23:32:41 +0300

    File updated at 8/10/2026, 11:32:40 PM

--- 21-MergeTwoSortedLists/21-MergeTwoSortedLists.java
+++ 21-MergeTwoSortedLists/21-MergeTwoSortedLists.java
@@ -1,15 +1,15 @@
-// Last updated: 8/10/2026, 11:20:09 PM
-class Solution {
-    public char findKthBit(int n, int k) {
-        if (n == 1) return '0';
-        
-        int len = (1 << n) - 1;
-        int mid = (len + 1) / 2;
-        
-        if (k == mid) return '1';
-        if (k < mid) return findKthBit(n - 1, k);
-        
-        char c = findKthBit(n - 1, len - k + 1);
-        return c == '0' ? '1' : '0';
-    }
-}
+// Last updated: 8/10/2026, 11:32:40 PM
+1class Solution {
+2    public char findKthBit(int n, int k) {
+3       if(n == 1)  return '0';
+4    
+5        int len  = (1<<n) - 1;
+6        int mid  = (len+1)/2;
+7
+8        if(mid == k) return '1';
+9        if(k < mid) return findKthBit(n-1,k);
+10
+11        char c = findKthBit(n-1,len-k+1);
+12        return c == '0'? '1':'0';
+13    }
+14}
