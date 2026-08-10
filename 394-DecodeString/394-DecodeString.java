6b68991fab20a2a3666374bbe5d39a3e8d292cd3
abdelrhamanosama <121834592+abdelrhamanosama@users.noreply.github.com>
2026-08-11 02:36:23 +0300

    File updated at 8/11/2026, 2:36:22 AM

--- 394-DecodeString/394-DecodeString.java
+++ 394-DecodeString/394-DecodeString.java
@@ -1,67 +1,40 @@
-// Last updated: 8/11/2026, 2:28:14 AM
-1
-2class Solution {
-3
-4    public String decodeString(String s) {
-5
-6        Stack<Integer> stNum = new Stack<>();
-7        Stack<StringBuilder> stText = new Stack<>();
-8
-9        StringBuilder current = new StringBuilder();
-10        int num = 0;
-11
-12        for (int i = 0; i < s.length(); i++) {
-13
-14            char c = s.charAt(i);
-15
-16            // Build the number
-17            if (Character.isDigit(c)) {
-18
-19                num = num * 10 + (c - '0');
-20            }
+// Last updated: 8/11/2026, 2:36:22 AM
+1class Solution {
+2    public String decodeString(String s) {
+3        Stack<StringBuilder> stText = new Stack<>();
+4        Stack<Integer> stNum = new Stack<>();
+5        Integer num = 0;
+6        StringBuilder current = new StringBuilder("");
+7        for (int i = 0; i < s.length(); i++) {
+8            char c = s.charAt(i);
+9             // Build the number 
+10            if (Character.isDigit(c)) {
+11                num = num * 10 + (c - '0');
+12            }
+13            else if(c == '['){
+14                stNum.push(num);
+15                stText.push(current);
+16                current = new StringBuilder("");
+17                num = 0;
+18            }
+19            else if(c == ']'){
+20                Integer times = stNum.pop();
 21
-22            // Start a new encoded section
-23            else if (c == '[') {
-24
-25                // Save the multiplier
-26                stNum.push(num);
-27
-28                // Save everything built before '['
-29                stText.push(current);
-30
-31                // Start building the content inside brackets
-32                current = new StringBuilder();
-33
-34                num = 0;
-35            }
-36
-37            // Finish an encoded section
-38            else if (c == ']') {
-39
-40                int times = stNum.pop();
-41
-42                StringBuilder previous = stText.pop();
-43
-44                StringBuilder decoded = new StringBuilder();
-45
-46                for (int j = 0; j < times; j++) {
-47                    decoded.append(current);
-48                }
-49
-50                // Append decoded part to the text before '['
-51                previous.append(decoded);
-52
-53                current = previous;
-54            }
-55
-56            // Normal character
-57            else {
-58
-59                current.append(c);
-60            }
-61        }
-62
-63        return current.toString();
-64    }
-65}
-66
+22                StringBuilder previous = stText.pop();
+23
+24                StringBuilder decoded = new StringBuilder();
+25
+26                for (Integer j = 0; j < times; j++) {
+27                    decoded.append(current);
+28                }
+29                previous.append(decoded);
+30                current = previous;   
+31            }
+32            else 
+33                {
+34                    current.append(c);
+35                }
+36        }
+37        return current.toString();
+38    }
+39}
