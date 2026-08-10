991067ef54569881b55115053d6bae11234d6968
abdelrhamanosama <121834592+abdelrhamanosama@users.noreply.github.com>
2026-08-10 23:20:10 +0300

    File updated at 8/10/2026, 11:20:09 PM

--- 21-MergeTwoSortedLists/21-MergeTwoSortedLists.java
+++ 21-MergeTwoSortedLists/21-MergeTwoSortedLists.java
@@ -1,23 +1,15 @@
-// Last updated: 8/10/2026, 11:17:35 PM
-1class Solution {
-2    
-3    public char findKthBit(int n, int k) {
-4        return work(n).charAt(k-1);
-5    }
-6
-7    private StringBuilder work(int n)
-8    {
-9        if(n == 1) return new StringBuilder("0");
-10
-11        StringBuilder sb = work(n-1);
-12        StringBuilder sbb = new StringBuilder(sb);
-13        sbb.append("1");
-14        for(int i = sb.length()- 1 ; i >= 0; i--)
-15            {
-16                if(sb.charAt(i) == '1') sbb.append("0");
-17                else sbb.append("1");
-18            }
-19        // System.out.println(sbb + "\t" + sb + "\t" + n);
-20        return sbb;
-21    }
-22}
+// Last updated: 8/10/2026, 11:20:09 PM
+class Solution {
+    public char findKthBit(int n, int k) {
+        if (n == 1) return '0';
+        
+        int len = (1 << n) - 1;
+        int mid = (len + 1) / 2;
+        
+        if (k == mid) return '1';
+        if (k < mid) return findKthBit(n - 1, k);
+        
+        char c = findKthBit(n - 1, len - k + 1);
+        return c == '0' ? '1' : '0';
+    }
+}
