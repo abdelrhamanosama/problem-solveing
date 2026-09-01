3d9ead54f5f1a80b3cd4b684abe620e4859d61eb
abdelrhamanosama <121834592+abdelrhamanosama@users.noreply.github.com>
2026-09-01 23:04:47 +0300

    File updated at 9/1/2026, 11:04:45 PM

--- 3568-MinimumMovestoCleantheClassroom/3568-MinimumMovestoCleantheClassroom.cpp
+++ 3568-MinimumMovestoCleantheClassroom/3568-MinimumMovestoCleantheClassroom.cpp
@@ -1,59 +1,137 @@
-// Last updated: 9/1/2026, 10:58:07 PM
+// Last updated: 9/1/2026, 11:04:45 PM
 1class Solution {
-2    static constexpr int dx[4] = {0, 1, 0, -1};
-3    static constexpr int dy[4] = {1, 0, -1, 0};
-4
-5public:
-6    int minMoves(vector<string>& classroom, int energy) {
-7        int m = classroom.size();
-8        int n = classroom[0].size();
-9        vector id(m, vector<int>(n));
-10        int sx, sy, cnt = 0;
-11        for (int i = 0; i < m; i++) {
-12            for (int j = 0; j < n; j++) {
-13                if (classroom[i][j] == 'S') {
-14                    sx = i;
-15                    sy = j;
-16                } else if (classroom[i][j] == 'L') {
-17                    id[i][j] = 1 << cnt++;
-18                }
-19            }
-20        }
-21
-22        vector bestEnergy(m, vector(n, vector<int>(1 << cnt, -1)));
-23        bestEnergy[sx][sy][0] = energy;
-24        struct Info {
-25            int x, y, mask, e, steps;
-26        };
-27        queue<Info> q;
-28        q.push({sx, sy, 0, energy, 0});
-29        while (!q.empty()) {
-30            Info t = q.front();
-31            q.pop();
-32            if (t.mask == (1 << cnt) - 1) {
-33                return t.steps;
-34            }
-35            if (t.e == 0) {
-36                continue;
-37            }
-38            for (int i = 0; i < 4; i++) {
-39                int nx = t.x + dx[i];
-40                int ny = t.y + dy[i];
-41
-42                if (nx < 0 || nx >= m || ny < 0 || ny >= n ||
-43                    classroom[nx][ny] == 'X') {
-44                    continue;
-45                }
-46
-47                int ne = classroom[nx][ny] == 'R' ? energy : t.e - 1;
-48                int nmask = t.mask | id[nx][ny];
-49
-50                if (ne > bestEnergy[nx][ny][nmask]) {
-51                    bestEnergy[nx][ny][nmask] = ne;
-52                    q.push({nx, ny, nmask, ne, t.steps + 1});
-53                }
-54            }
-55        }
-56        return -1;
-57    }
-58};
+2public:
+3
+4    int n, m;
+5    int fullMask;
+6    int maxEnergy;
+7
+8    int id[20][20];
+9
+10    int dx[4] = {1, -1, 0, 0};
+11    int dy[4] = {0, 0, 1, -1};
+12
+13    struct State {
+14        int x, y;
+15        int mask;
+16        int energy;
+17    };
+18
+19    int minMoves(vector<string>& classroom, int energy) {
+20
+21        n = classroom.size();
+22        m = classroom[0].size();
+23        maxEnergy = energy;
+24
+25        memset(id, -1, sizeof(id));
+26
+27        int cnt = 0;
+28        int sx = 0, sy = 0;
+29
+30        for (int i = 0; i < n; i++) {
+31            for (int j = 0; j < m; j++) {
+32
+33                if (classroom[i][j] == 'L') {
+34                    id[i][j] = cnt++;
+35                }
+36
+37                if (classroom[i][j] == 'S') {
+38                    sx = i;
+39                    sy = j;
+40                }
+41            }
+42        }
+43
+44        fullMask = (1 << cnt) - 1;
+45
+46        int masks = 1 << cnt;
+47
+48        // visited[x][y][mask][energy]
+49        int totalStates = n * m * masks * (energy + 1);
+50
+51        vector<bool> visited(totalStates, false);
+52
+53        auto getId = [&](int x, int y, int mask, int e) {
+54            return (((x * m + y) * masks + mask) * (energy + 1) + e);
+55        };
+56
+57        queue<State> q;
+58
+59        visited[getId(sx, sy, 0, energy)] = true;
+60
+61        q.push({sx, sy, 0, energy});
+62
+63        int moves = 0;
+64
+65        while (!q.empty()) {
+66
+67            int sz = q.size();
+68
+69            while (sz--) {
+70
+71                State cur = q.front();
+72                q.pop();
+73
+74                int x = cur.x;
+75                int y = cur.y;
+76                int mask = cur.mask;
+77                int e = cur.energy;
+78
+79                if (mask == fullMask) {
+80                    return moves;
+81                }
+82
+83                if (e == 0) {
+84                    continue;
+85                }
+86
+87                for (int d = 0; d < 4; d++) {
+88
+89                    int nx = x + dx[d];
+90                    int ny = y + dy[d];
+91
+92                    if (nx < 0 || nx >= n ||
+93                        ny < 0 || ny >= m) {
+94                        continue;
+95                    }
+96
+97                    if (classroom[nx][ny] == 'X') {
+98                        continue;
+99                    }
+100
+101                    int ne = e - 1;
+102                    int nmask = mask;
+103
+104                    // Clean trash
+105                    if (classroom[nx][ny] == 'L') {
+106                        nmask |= (1 << id[nx][ny]);
+107                    }
+108
+109                    // Recharge
+110                    if (classroom[nx][ny] == 'R') {
+111                        ne = maxEnergy;
+112                    }
+113
+114                    int stateId = getId(nx, ny, nmask, ne);
+115
+116                    if (visited[stateId]) {
+117                        continue;
+118                    }
+119
+120                    visited[stateId] = true;
+121
+122                    q.push({
+123                        nx,
+124                        ny,
+125                        nmask,
+126                        ne
+127                    });
+128                }
+129            }
+130
+131            moves++;
+132        }
+133
+134        return -1;
+135    }
+136};
