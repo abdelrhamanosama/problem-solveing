33595086f72b3338f6ec51d09bc52c4f9841ee38
abdelrhamanosama <121834592+abdelrhamanosama@users.noreply.github.com>
2026-09-06 14:22:08 +0300

    Create 1674-MinimumMovesToMakeArrayComplementary

---
+++ 1674-MinimumMovesToMakeArrayComplementary/1674-MinimumMovesToMakeArrayComplementary.java
@@ -0,0 +1,27 @@
+// Last updated: 9/6/2026, 2:22:07 PM
+class Solution {
+    public int minMoves(int[] nums, int limit) {
+        int n = nums.length;
+        int ans = (int)1e9;
+       int diff[] = new int[2*limit + 2];
+        for(int i = 0 ; i < n/2; i++){
+            int a = nums[i];
+            int b = nums[n-1-i];
+            int sum = a+b;
+            int low = Math.min(a,b) + 1;
+            int high = Math.max(a,b) + limit;
+            diff[2]+=2;
+            diff[2*limit + 1]-=2;
+            diff[low]-=1;
+            diff[high+1]+=1;
+            diff[sum]-=1;
+            diff[sum+1]+=1;
+        }
+        for(int i = 2; i <=2*limit; i++)
+            {
+                diff[i] +=diff[i-1];
+                ans = Math.min(diff[i] , ans); 
+            }
+            return ans;
+    }
+}
