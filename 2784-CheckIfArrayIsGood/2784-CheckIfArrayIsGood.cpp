2027e470ccd2670f7834854813a5b34807398a0a
abdelrhamanosama <121834592+abdelrhamanosama@users.noreply.github.com>
2026-09-06 14:17:12 +0300

    Create 2784-CheckIfArrayIsGood

---
+++ 2784-CheckIfArrayIsGood/2784-CheckIfArrayIsGood.cpp
@@ -0,0 +1,30 @@
+// Last updated: 9/6/2026, 2:17:11 PM
+/* صلِّ على النبي */
+#include <bits/stdc++.h>
+using namespace std;
+// #define int long long
+#define sz(s) (int)(s).size()
+#define all(s) s.begin(), s.end()
+
+class Solution {
+private:
+    // اللهم اهدنا يا رب اليك
+public:
+    bool isGood(vector<int>& nums) {
+        int mx = *max_element(all(nums));
+        map<int, int> mp;
+        for (auto x : nums) {
+            ++mp[x];
+            if (mp[x] == 1)
+                continue;
+            if (mp[x] == 2 && x == mx)
+                continue;
+            return 0;
+        }
+        if (mp[mx] == 2 && mp.size() == mx)
+
+            return 1;
+        else
+            return 0;
+    }
+};
