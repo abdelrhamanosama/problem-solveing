f6b3bad6fce46e5800fc9fcbfa2a166392934c19
abdelrhamanosama <121834592+abdelrhamanosama@users.noreply.github.com>
2026-09-06 14:16:23 +0300

    Create 3016-MinimumNumberOfPushesToTypeWordIi

---
+++ 3016-MinimumNumberOfPushesToTypeWordIi/3016-MinimumNumberOfPushesToTypeWordIi.cpp
@@ -0,0 +1,31 @@
+// Last updated: 9/6/2026, 2:16:22 PM
+class Solution {
+public:
+    int minimumPushes(string word) {
+        int n = word.size();
+        if(n <= 8) return n;
+        map<int,int>mp;
+        for(auto x: word) mp[x-'a']++;
+        vector<int> cnt;
+        for(auto x:mp)
+            cnt.push_back(x.second);
+        sort(cnt.begin() , cnt.end());
+        reverse(cnt.begin() , cnt.end());
+        // cout<<cnt.size();
+    
+        int sum = 0 , i = 0;
+        for(auto x:cnt){
+            if(i < 8)
+                sum+=x;
+            else if(i < 16)
+                sum+=(x*2);
+            else if(i<24)
+                sum+=(x*3);
+            else 
+                sum+=(x*4);
+            i++;
+            cout<<x<<" ";
+        }
+        return sum;
+    }
+};
