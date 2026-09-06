96f0c0f326cafe77cdcc5c0d5b9a825b3faac564
abdelrhamanosama <121834592+abdelrhamanosama@users.noreply.github.com>
2026-09-06 14:13:11 +0300

    Create 3660-JumpGameIx

---
+++ 3660-JumpGameIx/3660-JumpGameIx.cpp
@@ -0,0 +1,33 @@
+// Last updated: 9/6/2026, 2:13:09 PM
+class Solution
+{
+private:
+    // اللهم اهدنا يا رب اليك
+   
+public:
+    vector<int> maxValue(vector<int> &nums)
+    {
+
+        int n = nums.size();
+        vector<int> pref(n, 0), suff(n, 0);
+        pref[0] = nums[0];
+        for (int i = 1; i < n; i++)
+        {
+            pref[i] = max(pref[i - 1], nums[i]);
+        }
+        suff[n-1] = nums[n-1];
+        for(int i = n-2 ; i>= 0 ; i--){
+            suff[i] = min(suff[i+1] , nums[i]);
+        }
+
+        vector<int> ans(n);
+        ans[n-1] = pref[n-1];
+        for(int i = n-2 ; i >= 0; i--){
+            if(pref[i] > suff[i+1]) 
+                ans[i] = max(pref[i],ans[i+1]);
+            else 
+                ans[i] = pref[i];
+        }
+        return ans;
+    }
+};
