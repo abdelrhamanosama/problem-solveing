37e53668c15bbb74b0588b44aa11643f846648e2
abdelrhamanosama <121834592+abdelrhamanosama@users.noreply.github.com>
2026-09-06 14:19:14 +0300

    Create 2145-CountTheHiddenSequences

---
+++ 2145-CountTheHiddenSequences/2145-CountTheHiddenSequences.cpp
@@ -0,0 +1,16 @@
+// Last updated: 9/6/2026, 2:19:13 PM
+class Solution {
+public:
+    int numberOfArrays(vector<int>& differences, int lower, int upper) {
+        int x = 0, y = 0, cur = 0;
+        for (int d : differences) {
+            cur += d;
+            x = min(x, cur);
+            y = max(y, cur);
+            if (y - x > upper - lower) {
+                return 0;
+            }
+        }
+        return (upper - lower) - (y - x) + 1;
+    }
+};
