cc5b4786bcac89c83c8e25522130c0b8054508a0
abdelrhamanosama <121834592+abdelrhamanosama@users.noreply.github.com>
2026-09-06 16:01:09 +0300

    Create 796-RotateString

---
+++ 796-RotateString/796-RotateString.cpp
@@ -0,0 +1,17 @@
+// Last updated: 9/6/2026, 4:01:10 PM
+class Solution {
+public:
+    bool rotateString(string s, string goal) {
+        if(s == goal) return 1;
+        for(int i  = 0 ; i < s.size(); i++){
+            string try1 = makesShifting(i,s);
+            if(try1 == goal) return 1;
+        }
+        return 0;
+    }
+    string makesShifting(int i  , string s){
+       string s1 =  s.substr(0, i);
+       string s2 =  s.substr(i);
+       return s2 + s1;
+    }
+};
