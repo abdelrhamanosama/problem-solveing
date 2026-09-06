ed9aacc7dd0c92a7b3c24d0c3cf73360be70fa37
abdelrhamanosama <121834592+abdelrhamanosama@users.noreply.github.com>
2026-09-06 16:00:20 +0300

    Create 994-RottingOranges

---
+++ 994-RottingOranges/994-RottingOranges.cpp
@@ -0,0 +1,59 @@
+// Last updated: 9/6/2026, 4:00:20 PM
+/* صلِّ على النبي */
+
+
+class Solution
+{
+public:
+    int orangesRotting(vector<vector<int>> &grid)
+    {
+        int n = grid.size();
+        int m = grid[0].size();
+        queue<pair<int,int>> q; //  multi source BFS // (x,y,cost)
+        int fresh = 0 , minutes = 0;
+
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < m; j++)
+            {
+                if (grid[i][j] == 2)
+                {
+                    q.push({i, j});
+                }
+                else if (grid[i][j] == 1)
+                    fresh++;
+            }
+        if (fresh == 0)
+            return 0;
+        if (q.empty())
+            return -1;
+        int mx = 0;
+        int dx[] = {1, -1, 0, 0};
+        int dy[] = {0, 0, 1, -1};
+        auto valid = [&](int x, int y) -> bool
+        {
+            return x >= 0 && y >= 0 && x < n && y < m;
+        };
+        while (!q.empty() && fresh > 0)
+        {   
+            int sz = q.size();
+            minutes++;
+           while(sz--){
+            auto [px, py] = q.front();
+            q.pop();
+            for (int i = 0; i < 4; i++)
+            {
+                int nx = px + dx[i];
+                int ny = py + dy[i];
+                if(!valid(nx,ny)) continue;
+                if(grid[nx][ny] != 1) continue;
+                q.push({nx,ny});
+                grid[nx][ny] = 2;
+                fresh--;
+            }
+           }
+        }
+        if (fresh)
+            return -1;
+        return minutes;
+    }
+};
