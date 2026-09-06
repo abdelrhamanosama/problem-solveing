ee2dc248d2a218f74db7c17a85a534add89c6dbb
abdelrhamanosama <121834592+abdelrhamanosama@users.noreply.github.com>
2026-09-06 14:16:12 +0300

    Create 3043-FindTheLengthOfTheLongestCommonPrefix

---
+++ 3043-FindTheLengthOfTheLongestCommonPrefix/3043-FindTheLengthOfTheLongestCommonPrefix.cpp
@@ -0,0 +1,39 @@
+// Last updated: 9/6/2026, 2:16:11 PM
+/* صلِّ على النبي */
+#include <bits/stdc++.h>
+using namespace std;
+// #define int long long
+#define sz(s) (int)(s).size()
+#define all(s) s.begin(), s.end()
+
+class Solution
+{
+private:
+    // اللهم اهدنا يا رب اليك
+public:
+    int longestCommonPrefix(vector<int> &arr1, vector<int> &arr2)
+    {
+        unordered_set<string> s;
+        for (int i = 0; i < arr1.size(); i++)
+        {
+            string st = to_string(arr1[i]), curr = "";
+            for (auto x : st)
+            {
+                curr += x;
+                s.insert(curr);
+            }
+        }
+        int mx = 0;
+        for (int i = 0; i < arr2.size(); i++)
+        {
+            string st = to_string(arr2[i]), curr = "";
+            int j = 0;
+            for (auto x : st)
+            {
+                curr += x;
+                if(s.find(curr)!=s.end()) mx = max(mx , ++j);
+            }
+        }
+        return mx;
+    }
+};
