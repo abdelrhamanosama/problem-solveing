4c57e3977e073d9c8505f6d638d03d3e6ca5c1d4
abdelrhamanosama <121834592+abdelrhamanosama@users.noreply.github.com>
2026-09-06 14:17:21 +0300

    Create 2799-CountCompleteSubarraysInAnArray

---
+++ 2799-CountCompleteSubarraysInAnArray/2799-CountCompleteSubarraysInAnArray.cpp
@@ -0,0 +1,33 @@
+// Last updated: 9/6/2026, 2:17:20 PM
+class Solution {
+public:
+    int countCompleteSubarrays(vector<int>& nums) {
+        map<int,int>mp;
+        set<int>st1,st2;
+        int n=nums.size();
+        for(int i=0;i<nums.size();i++)
+        {
+            st1.insert(nums[i]);
+        }
+        int k = st1.size();
+        int cnt = 0;
+        int last = 0;
+        for(int i = 0, j = 0; i < n; i++) 
+            {
+                mp[nums[i]]++;
+                if(mp.size() == k){
+                        
+                        while(mp.size()==k){
+                            cnt+=n-i;
+                            mp[nums[j]]--;
+                            if(!mp[nums[j]]){
+                                mp.erase(nums[j]);
+                            }
+                            j++;
+                        }
+                    // last = i;
+                }
+            }
+        return cnt;
+    }
+};
