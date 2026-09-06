d6cf175a747ecb00e58792d9f4389ef236c27d4e
abdelrhamanosama <121834592+abdelrhamanosama@users.noreply.github.com>
2026-09-06 14:19:21 +0300

    Create 2119-ANumberAfterADoubleReversal

---
+++ 2119-ANumberAfterADoubleReversal/2119-ANumberAfterADoubleReversal.java
@@ -0,0 +1,19 @@
+// Last updated: 9/6/2026, 2:19:20 PM
+class Solution {
+    public boolean isSameAfterReversals(int num) {
+        return (num == reverse(reverse(num)));
+    }
+    public int reverse(int num) {
+        num = Math.abs(num);
+        int numOfDigit = (int)Math.log10(num) + 1;
+        int product = 0;
+            for(int i = 1 ; i <= numOfDigit ; i++) //2147483647
+                {
+                    int digit = (num % 10);
+                    product += digit * (int)Math.pow(10 , numOfDigit - i);
+                    num /= 10;
+                }
+        return(product);
+    }
+
+}
