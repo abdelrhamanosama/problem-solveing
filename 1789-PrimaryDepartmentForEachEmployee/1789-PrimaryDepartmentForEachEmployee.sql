57a151e81d4553836855620b5bf5258074c4e787
abdelrhamanosama <121834592+abdelrhamanosama@users.noreply.github.com>
2026-09-06 14:21:25 +0300

    Create 1789-PrimaryDepartmentForEachEmployee

---
+++ 1789-PrimaryDepartmentForEachEmployee/1789-PrimaryDepartmentForEachEmployee.sql
@@ -0,0 +1,17 @@
+-- Last updated: 9/6/2026, 2:21:24 PM
+# Write your MySQL query statement below
+
+SELECT
+employee_id, department_id
+FROM Employee 
+WHERE primary_flag = 'Y'
+#GROUP BY employee_id
+#HAVING count(department_id) > 1
+
+UNION ALL
+
+SELECT
+employee_id, department_id
+FROM Employee 
+GROUP BY employee_id
+HAVING count(department_id) = 1
