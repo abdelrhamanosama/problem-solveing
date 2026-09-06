15434b33fd485860c90f544ef82f7dae4781b0f0
abdelrhamanosama <121834592+abdelrhamanosama@users.noreply.github.com>
2026-09-06 14:17:20 +0300

    Create 2762-ContinuousSubarrays

---
+++ 2762-ContinuousSubarrays/2762-ContinuousSubarrays.cpp
@@ -0,0 +1,22 @@
+// Last updated: 9/6/2026, 2:17:19 PM
+class Solution {
+public:
+    long long continuousSubarrays(vector<int>& a) {
+    map<int,int>mp;
+    int i = 0, j = 0;
+    long long count = 0;
+    for(;i < a.size();i++){
+        mp[a[i]]++;
+        while(abs(mp.rbegin()->first - mp.begin()->first) > 2 && j < i) // the biggest - the smallest
+        {
+            mp[a[j]]--;
+            if(!mp[a[j]])
+                mp.erase(a[j]);
+            j++;
+        }
+        count += i  - j + 1;
+    }
+    return count;
+}
+};
+auto speed = [](){cin.tie(0)->sync_with_stdio(0);return true;}();
