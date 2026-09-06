4cd70cfdc565c4a9edbfab8bd4fa5eee58767f09
abdelrhamanosama <121834592+abdelrhamanosama@users.noreply.github.com>
2026-09-06 14:18:02 +0300

    Create 2537-CountTheNumberOfGoodSubarrays

---
+++ 2537-CountTheNumberOfGoodSubarrays/2537-CountTheNumberOfGoodSubarrays.cpp
@@ -0,0 +1,35 @@
+// Last updated: 9/6/2026, 2:18:01 PM
+#pragma GCC optimize("Ofast","inline","fast-math","unroll-loops","no-stack-protector","-ffast-math")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,tune=native","f16c")
+static const bool Booster = [](){
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    std::cout.tie(nullptr);
+    return true;
+}();
+class Solution {
+public:
+        long long countGood(vector<int>& nums, int k) {
+        long long counter = 0, totalPairs = 0;
+        int i, j = 0, n = nums.size();
+        map<int, int>mp;
+        for (int i = 0; i < n; i++)
+        {
+            totalPairs -= mp[nums[i]]*(mp[nums[i]]-1)/2;
+            mp[nums[i]]++;
+            totalPairs += mp[nums[i]]*(mp[nums[i]]-1)/2;
+            if(totalPairs >= k) {
+                // counter++;
+                while(totalPairs >= k && j < i){
+                    counter+=(n - i);
+                    totalPairs -= mp[nums[j]]*(mp[nums[j]]-1)/2;
+                    mp[nums[j]]--;
+                    totalPairs += mp[nums[j]]*(mp[nums[j]]-1)/2;
+                    j++;
+                    
+            }
+            }
+        }
+        return counter;  
+    }
+};
