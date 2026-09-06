b795ed22b60e94a71a52de3383cce63d6ff891a7
abdelrhamanosama <121834592+abdelrhamanosama@users.noreply.github.com>
2026-09-06 16:00:34 +0300

    Create 948-BagOfTokens

---
+++ 948-BagOfTokens/948-BagOfTokens.java
@@ -0,0 +1,21 @@
+// Last updated: 9/6/2026, 4:00:34 PM
+import java.util.Arrays;
+
+class Solution {
+    public int bagOfTokensScore(int[] A, int P) {
+        Arrays.sort(A);
+        int res = 0, points = 0, i = 0, j = A.length - 1;
+        while(i <= j ){
+            if(P>=A[i]) {
+                P -= A[i++];
+                res = Math.max(res,++points);
+            }else  if(points > 0){
+                points --;
+                P += A[j--];
+            } else {
+                break;
+            }
+        }
+        return  res;
+    }
+}
