06d5d152c239822db5f031a007cab7e2aa63d298
abdelrhamanosama <121834592+abdelrhamanosama@users.noreply.github.com>
2026-09-06 14:18:43 +0300

    Create 2257-CountUnguardedCellsInTheGrid

---
+++ 2257-CountUnguardedCellsInTheGrid/2257-CountUnguardedCellsInTheGrid.cpp
@@ -0,0 +1,37 @@
+// Last updated: 9/6/2026, 2:18:42 PM
+class Solution {
+public:
+    int countUnguarded(int m, int n, vector<vector<int>>& guards, vector<vector<int>>& walls) {
+        vector<vector<int>> g(m, vector<int>(n, 0)); 
+        
+        for(auto& x : walls) g[x[0]][x[1]] = 1; // wall = 1
+        for(auto& x : guards) g[x[0]][x[1]] = 3; // guard = 3
+
+        for(auto& x : guards) {
+            int r = x[0], c = x[1];
+            
+            // up
+            for(int i = r - 1; i >= 0 && g[i][c] != 1 && g[i][c] != 3; i--) 
+                g[i][c] = 2;
+            
+            // down
+            for(int i = r + 1; i < m && g[i][c] != 1 && g[i][c] != 3; i++) 
+                g[i][c] = 2;
+            
+            // left
+            for(int j = c - 1; j >= 0 && g[r][j] != 1 && g[r][j] != 3; j--) 
+                g[r][j] = 2;
+            
+            // right
+            for(int j = c + 1; j < n && g[r][j] != 1 && g[r][j] != 3; j++) 
+                g[r][j] = 2;
+        }
+
+        int cnt = 0;
+        for(int i = 0; i < m; i++) 
+            for(int j = 0; j < n; j++) 
+                if(g[i][j] == 0) cnt++;
+
+        return cnt;
+    }
+};
