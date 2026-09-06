4af3ac8b15a3b06cc86ec555542f67a4e21cb98b
abdelrhamanosama <121834592+abdelrhamanosama@users.noreply.github.com>
2026-09-06 14:23:19 +0300

    Create 1513-NumberOfSubstringsWithOnly1s

---
+++ 1513-NumberOfSubstringsWithOnly1s/1513-NumberOfSubstringsWithOnly1s.cpp
@@ -0,0 +1,26 @@
+// Last updated: 9/6/2026, 2:23:18 PM
+class Solution {
+public:
+    int mod = 1e9 + 7;
+    int numSub(string s) {
+        return rle(s);
+    }
+    int rle( string s) {
+    long long ans = 0;
+    long long count = 1;
+    for (int i = 1; i < (int)s.size(); i++) {
+        if (s[i] == s[i - 1]) {
+            count++;
+        } else {
+            if(s[i-1] == '1')
+                ans += (count * (count + 1)/2)%mod;
+            ans %= mod; 
+            count = 1;
+        }
+    }
+    if(s.back() == '1') 
+    ans += (count * (count + 1)/2)%mod;
+    ans %= mod; 
+    return ans;
+}
+};
