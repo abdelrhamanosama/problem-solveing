5e3d471f6ef0c275138ef383a65523fce871289f
abdelrhamanosama <121834592+abdelrhamanosama@users.noreply.github.com>
2026-09-06 14:16:01 +0300

    Create 3105-LongestStrictlyIncreasingOrStrictlyDecreasingSubarray

---
+++ 3105-LongestStrictlyIncreasingOrStrictlyDecreasingSubarray/3105-LongestStrictlyIncreasingOrStrictlyDecreasingSubarray.cpp
@@ -0,0 +1,37 @@
+// Last updated: 9/6/2026, 2:16:00 PM
+class Solution {
+public:
+    int longestMonotonicSubarray(vector<int>& nums) {
+    int maxx = -1, counter = 0;
+    for(int i = 0; i < nums.size()-1;i++){
+        if(nums[i+1] > nums[i]){
+            if(counter)
+                counter++;
+            else
+                counter+=2;
+        }
+        else{
+            maxx = max(maxx,counter);
+            // cout<<maxx<<" ";
+            counter = 0;
+        }
+    }
+    maxx = max(maxx,counter);
+    counter = 0;
+    for(int i = 0; i < nums.size()-1;i++){
+        if(nums[i+1] < nums[i]){
+            if(counter)
+                counter++;
+            else
+                counter+=2;
+        }
+        else{
+            maxx = max(maxx,counter);
+            // cout<<maxx<<" ";
+            counter = 0;
+        }
+    }
+    maxx = max(maxx,counter);
+    return maxx!=0? maxx:1;
+}
+};
