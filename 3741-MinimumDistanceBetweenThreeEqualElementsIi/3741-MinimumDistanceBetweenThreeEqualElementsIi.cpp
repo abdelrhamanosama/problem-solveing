4ff12fbc7759177992b246389c73d5ca5c99318f
abdelrhamanosama <121834592+abdelrhamanosama@users.noreply.github.com>
2026-09-06 14:12:37 +0300

    Create 3741-MinimumDistanceBetweenThreeEqualElementsIi

---
+++ 3741-MinimumDistanceBetweenThreeEqualElementsIi/3741-MinimumDistanceBetweenThreeEqualElementsIi.cpp
@@ -0,0 +1,18 @@
+// Last updated: 9/6/2026, 2:12:35 PM
+class Solution {
+public:
+    int minimumDistance(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> second_last(n+3,-1);
+        vector<int> last(n+3,-1);
+        int ans = 1<<30;
+        for(int i = 0 ;i < n; i++){
+            if(~second_last[nums[i]]) {
+                ans = min(ans , (i-second_last[nums[i]])*2);
+            }
+            second_last[nums[i]] = last[nums[i]];
+            last[nums[i]] = i;
+        }
+        return (ans==(1<<30) ? -1 : ans);
+    }
+};
