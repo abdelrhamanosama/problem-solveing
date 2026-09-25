a1180b4bded4e6231c9d91e205b2aa0503d7add4
abdelrhamanosama <121834592+abdelrhamanosama@users.noreply.github.com>
2026-09-25 07:48:31 +0300

    File updated at 9/25/2026, 7:48:31 AM

--- 20-ValidParentheses/20-ValidParentheses.cpp
+++ 20-ValidParentheses/20-ValidParentheses.cpp
@@ -1,14 +1,35 @@
-// Last updated: 9/25/2026, 7:25:22 AM
+// Last updated: 9/25/2026, 7:48:31 AM
 1class Solution {
 2public:
-3    bool isValid(string s) {
-4       stack<char> st;
-5        for(int i = 0 ; i< s.size();i ++){
-6            if(st.empty()) st.push(s[i]);
-7            else if(st.top() == '(' && s[i] == ')' || st.top() == '[' && s[i] == ']' || st.top() == '{' && s[i] == '}' ) st.pop();
-8
-9            else  st.push(s[i]);
-10        } 
-11        return st.size() == 0;
-12    }
-13};
+3    string removeDuplicates(string s, int k) {
+4        stack<pair<char, int>> st;
+5
+6        for (char c : s) {
+7            if (!st.empty() && st.top().first == c) {
+8                st.top().second++;
+9
+10                if (st.top().second == k) {
+11                    st.pop();
+12                }
+13            }
+14            else {
+15                st.push({c, 1});
+16            }
+17        }
+18
+19        string ans;
+20
+21        while (!st.empty()) {
+22            auto [c, cnt] = st.top();
+23            st.pop();
+24
+25            while (cnt--) {
+26                ans += c;
+27            }
+28        }
+29
+30        reverse(ans.begin(), ans.end());
+31
+32        return ans;
+33    }
+34};
