791cdb976658978e1ae5e4f116b2b88e805f0fdb
abdelrhamanosama <121834592+abdelrhamanosama@users.noreply.github.com>
2026-09-25 09:57:35 +0300

    File updated at 9/25/2026, 9:57:34 AM

--- 1096-BraceExpansionII/1096-BraceExpansionII.cpp
+++ 1096-BraceExpansionII/1096-BraceExpansionII.cpp
@@ -1,79 +1,65 @@
-// Last updated: 9/25/2026, 9:41:33 AM
-1
-2class Solution {
-3    string s;
-4    int pos;
-5
-6    set<string> concat(const set<string>& A,
-7                       const set<string>& B) {
-8
-9        set<string> res;
+// Last updated: 9/25/2026, 9:57:34 AM
+1/* صلِّ على النبي */
+2#include <bits/stdc++.h>
+3
+4#include <ext/pb_ds/assoc_container.hpp>
+5using namespace __gnu_pbds;
+6using namespace std;
+7#define ll long long
+8#define sz(s) (int)(s).size()
+9#define all(s) s.begin(), s.end()
 10
-11        for (const string& a : A) {
-12            for (const string& b : B) {
-13                res.insert(a + b);
-14            }
-15        }
-16
-17        return res;
-18    }
-19
-20    set<string> parseFactor() {
-21
-22        if (s[pos] == '{') {
-23            pos++; // {
-24
-25            set<string> res = parseExpression();
-26
-27            pos++; // }
-28
-29            return res;
+11
+12class Solution {
+13public:
+14    int pos ;
+15    set<string> concate(set<string> a, set<string> b){
+16        // cartesien product
+17        set<string> s;
+18        for(auto x:a)
+19            for(auto y:b)
+20                s.insert(x+y);
+21        return s;
+22    }
+23
+24    set<string> ParseFactor(string s){
+25        set<string> res;
+26        if(s[pos] == '{'){
+27            pos++; // handle starting { 
+28            res = ParseExpr(s); // just handle  , don't care curly beraces
+29            pos++; // handle ending }
 30        }
-31
-32        return {string(1, s[pos++])};
-33    }
-34
-35    set<string> parseTerm() {
-36
-37        set<string> res = {""};
-38
-39        while (pos < s.size() &&
-40               s[pos] != ',' &&
-41               s[pos] != '}') {
-42
-43            set<string> cur = parseFactor();
-44
-45            res = concat(res, cur);
-46        }
-47
-48        return res;
-49    }
-50
-51    set<string> parseExpression() {
-52
-53        set<string> res = parseTerm();
+31        else 
+32            res.insert(string(1, s[pos++]));
+33        return res;
+34    }
+35    set<string> ParseTerm(string s){
+36        set<string> res = {""};
+37
+38        while(pos < s.size() 
+39        && s[pos] != ','  // go to expression to handle next unoin expression
+40        && s[pos] !='}' // end orginal expresion
+41        ){
+42            set<string> cur = ParseFactor(s);
+43            res = concate(res , cur);
+44        }
+45        return res;
+46    }
+47    set<string> ParseExpr(string s){
+48        set<string> res = ParseTerm(s);
+49        while(pos < s.size() && s[pos] == ','){
+50            pos++;
+51            set<string> cur = ParseTerm(s);
+52            res.insert(all(cur));
+53        }
 54
-55        while (pos < s.size() && s[pos] == ',') {
-56
-57            pos++; // ,
-58
-59            set<string> cur = parseTerm();
-60
-61            res.insert(cur.begin(), cur.end());
-62        }
-63
-64        return res;
-65    }
-66
-67public:
-68    vector<string> braceExpansionII(string expression) {
-69
-70        s = expression;
-71        pos = 0;
-72
-73        set<string> res = parseExpression();
-74
-75        return vector<string>(res.begin(), res.end());
-76    }
-77};
-78
+55        return res;
+56    }
+57
+58    vector<string> braceExpansionII(string expression) {
+59        pos = 0;
+60        set<string> s = ParseExpr(expression);
+61
+62        return vector<string>(all(s));
+63    }
+64};
