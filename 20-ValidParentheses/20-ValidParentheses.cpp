9dd4cf1cec7a480ada14eab873dda47763ff7cc2
abdelrhamanosama <121834592+abdelrhamanosama@users.noreply.github.com>
2026-09-25 08:01:28 +0300

    File updated at 9/25/2026, 8:01:28 AM

--- 20-ValidParentheses/20-ValidParentheses.cpp
+++ 20-ValidParentheses/20-ValidParentheses.cpp
@@ -1,35 +1,43 @@
-// Last updated: 9/25/2026, 7:48:31 AM
+// Last updated: 9/25/2026, 8:01:28 AM
 1class Solution {
-2public:
-3    string removeDuplicates(string s, int k) {
-4        stack<pair<char, int>> st;
-5
-6        for (char c : s) {
-7            if (!st.empty() && st.top().first == c) {
-8                st.top().second++;
-9
-10                if (st.top().second == k) {
-11                    st.pop();
-12                }
-13            }
-14            else {
-15                st.push({c, 1});
-16            }
-17        }
-18
-19        string ans;
-20
-21        while (!st.empty()) {
-22            auto [c, cnt] = st.top();
-23            st.pop();
+2    unordered_map<TreeNode*, long long> mp;
+3    TreeNode* mainRoot;
+4
+5    long long dfs1(TreeNode* root) {
+6        if (root == nullptr)
+7            return 0;
+8
+9        long long sum = root->val
+10                      + dfs1(root->left)
+11                      + dfs1(root->right);
+12
+13        mp[root] = sum;
+14
+15        return sum;
+16    }
+17
+18    void dfs2(TreeNode* root, long long& mx) {
+19        if (root == nullptr)
+20            return;
+21
+22        dfs2(root->left,mx);
+23        dfs2(root->right,mx);
 24
-25            while (cnt--) {
-26                ans += c;
-27            }
-28        }
-29
-30        reverse(ans.begin(), ans.end());
-31
-32        return ans;
-33    }
-34};
+25        long long sub = mp[root];
+26        long long other = mp[mainRoot] - sub;
+27
+28        mx = max(mx, sub * other);
+29    }
+30
+31public:
+32    int maxProduct(TreeNode* root) {
+33        mainRoot = root;
+34
+35        dfs1(root);
+36
+37        long long mx = 0;
+38        dfs2(root, mx);
+39
+40        return mx % 1'000'000'007;
+41    }
+42};
